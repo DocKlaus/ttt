060cd997daea29ff9ce844ce758f279a48eb7548
Любовь <189024783+DocKlaus@users.noreply.github.com>
2025-07-07 16:08:32 +0300

    The grid can be filled with symbols ('X' and 'O')

--- UI/window.py
+++ UI/window.py
@@ -1,22 +1,38 @@
 import tkinter as tk
-from tkinter import messagebox
 
-def create_board():
-    root = tk.Tk()
-    root.title('Field')
 
-    for i in range(9):
-        button = tk.Button(
-            root,
-            text='',
-            font=('Arial', 15),
-            width=5,
-            height=2,
-            bg='#f0f0f0',
-            relief='ridge'
-        )
-        button.grid(row=i // 3, column=i % 3, padx=2, pady=2)
+class TTTUI:
+    def __init__(self, root):
+        self.root = root
+        self.root.title('Field')
+        self.current_player = 'X'
+        self.board = [''] * 9
 
-    root.mainloop()
+        self.buttons = []
+        for i in range(9):
+            button = tk.Button(
+                root,
+                text='',
+                font=('Arial', 15),
+                width=5,
+                height=2,
+                bg='#f0f0f0',
+                relief='ridge',
+                command=lambda idx=i: self.on_click(idx)
+            )
+            button.grid(row=i // 3, column=i % 3, padx=2, pady=2)
+            self.buttons.append(button)
 
-create_board()
+    def on_click(self, idx):
+        if self.board[idx] != '':
+            return
+
+        self.board[idx] = self.current_player
+        self.buttons[idx].config(text=self.current_player)
+
+        self.current_player = 'O' if self.current_player == 'X' else 'X'
+
+
+root = tk.Tk()
+game = TTTUI(root)
+root.mainloop()
